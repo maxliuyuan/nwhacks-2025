fb92dc60474f9a566aa13de40ae0961f6bb47678
mla387 <mla387@sfu.ca>
2025-01-18 22:41:16 -0800

    threading stuff

--- main.py
+++ main.py
@@ -2,6 +2,10 @@ import serial
 from fastapi import FastAPI
 import uvicorn
 from enum import Enum
+import threading
+import os
+import signal
+
 
 class Listening(Enum):
     OFF = 0
@@ -9,6 +13,7 @@ class Listening(Enum):
 
 class Ducky:
     def __init__(self, port, baud_rate):
+        self.terminate = False
         self.ser = serial.Serial(port, baud_rate)
         self.listening_state = Listening.OFF
         self.app = FastAPI()
@@ -33,19 +38,47 @@ class Ducky:
 
     def listen(self):
         print("Listening for button presses...")
-        try:
-            while True:
-                if self.ser.in_waiting > 0:
-                    line = self.ser.readline().decode('utf-8').strip()
-                    print(f"Received: {line}")
-                    if line == "BUTTON_PRESS":
-                        self.toggle_listening()
-                        print("Listening:", self.listening_state.name)
-        except KeyboardInterrupt:
-            print("\nExiting...")
-        finally:
-            self.ser.close()
-        
+        while True:
+            if self.ser.in_waiting > 0:
+                line = self.ser.readline().decode('utf-8').strip()
+                print(f"Received: {line}")
+                if line == "BUTTON_PRESS":
+                    self.toggle_listening()
+                    print("Listening:", self.listening_state.name)
+            if (self.terminate):
+                self.ser.write('0'.encode())
+                print("\nExiting...")
+                self.ser.close()
+                break
+
+    def userInput(self):
+        while (True):
+            try:
+                user_input = input()
+                if (user_input == "shutdown"):  # add sigint
+                    self.terminate = True
+                    break
+            except:
+                self.terminate = True
+                break
+    
+    def kms(self):
+        os.kill(os.getpid(), signal.SIGINT)
+
+    
+    def run(self):
+        listen_thread = threading.Thread(target=self.listen)
+        uvicorn_thread = threading.Thread(target=lambda : uvicorn.run(self.app, host="127.0.0.1", port=8000))
+        input_thread = threading.Thread(target=self.userInput)
+
+        listen_thread.start()
+        uvicorn_thread.start()
+        input_thread.start()
+
+
+        listen_thread.join()
+        input_thread.join()
+        self.kms()
 
 # Usage
 arduino_port = 'COM3'  # Change this to your Arduino's port
@@ -55,5 +88,4 @@ app = ducky.app
 
 # Run the application (if runninrg the script directly)
 if __name__ == "__main__":
-    uvicorn.run(app, host="127.0.0.1", port=8000)
-    # ducky.listen()
+    ducky.run()
