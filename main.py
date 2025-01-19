6f5ebec43d262111b1acf94a9477d1e4270fe562
iaj2 <108596576+iaj2@users.noreply.github.com>
2025-01-18 21:40:42 -0800

    api

--- main.py
+++ main.py
@@ -1,15 +1,28 @@
 import serial
+from fastapi import FastAPI
 from enum import Enum
 
 class Listening(Enum):
     OFF = 0
     ON = 1
 
-class ButtonListener:
+class Ducky:
     def __init__(self, port, baud_rate):
         self.ser = serial.Serial(port, baud_rate)
         self.listening_state = Listening.OFF
+        self.app = FastAPI()
+        self.setup_routes()
 
+    def setup_routes(self):
+        @self.app.get("/")
+        def read_root():
+            print("Ducky says hi")
+            return
+
+        @self.app.get("/move-ducky")
+        def move_ducky(state: int):
+            self.ser.write(state)
+                
     def toggle_listening(self):
         if self.listening_state == Listening.OFF:
             self.listening_state = Listening.ON
@@ -30,15 +43,16 @@ class ButtonListener:
             print("\nExiting...")
         finally:
             self.ser.close()
-
-    def write(self):
-        while True:
-            userInput = input()
-            self.ser.write(userInput.encode())
+        
 
 # Usage
 arduino_port = 'COM3'  # Change this to your Arduino's port
 baud_rate = 9600
-listener = ButtonListener(arduino_port, baud_rate)
-# listener.listen()
-listener.write()
+ducky = Ducky(arduino_port, baud_rate)
+app = ducky.app
+
+# Run the application (if runninrg the script directly)
+if __name__ == "__main__":
+    import uvicorn
+    uvicorn.run(app, host="127.0.0.1", port=8000)
+    ducky.listen()
