3d248a123d6b9761d8aa1263bd2f7bdb2a3a7658
Sophia <66497192+sophiadt@users.noreply.github.com>
2025-01-19 02:57:14 -0800

    remove not needed code

--- frontend/components/Call.tsx
+++ frontend/components/Call.tsx
@@ -26,16 +26,15 @@ const Call = ({
 }) => {
   const [isCalling, setIsCalling] = useState(false);
   const [loading, setLoading] = useState(true);
-  const [showHangUpButton, setShowHangUpButton] = useState(false);
-  const [showListeningText, setShowListeningText] = useState(false);
   const [transcriptContent, setTranscriptContent] = useState<string>("");
   const [callId, setCallId] = useState<string>("");
   const [isLoadingAnalysis, setIsLoadingAnalysis] = useState(false);
 
   const url = "http://127.0.0.1:8000";
 
+  // CHANGE YOUR API KEY HERE
   const retellClient = new Retell({
-    apiKey: "key_ce6b06ad98cb7b99a09d84a5f733",
+    apiKey: "key_97507ab19561bc73916f72b04170",
   });
 
   console.log("apiKey", retellClient.apiKey);
@@ -83,7 +82,6 @@ const Call = ({
       console.log("call ended");
       setIsCalling(false);
       onAgentTalkingChange(false, "");
-      setShowListeningText(false);
       endCall();
     });
 
@@ -129,18 +127,6 @@ const Call = ({
     });
   }, [startCall]);
 
-  // Handle the loading state delay and show listening text after startCall is true
-  useEffect(() => {
-    if (startCall) {
-      const timer = setTimeout(() => {
-        setLoading(false);
-        // setShowListeningText(true);
-      }, 4000);
-
-      return () => clearTimeout(timer);
-    }
-  }, [startCall]);
-
   // Fetch the call analysis once the callId is set and the call has ended
   // useEffect(() => {
   //   const fetchAnalysis = async () => {
@@ -155,24 +141,6 @@ const Call = ({
   //     // fetchAnalysis();
   //   }
   // }, [callId, isCalling]);
-
-  const toggleConversation = async () => {
-    if (isCalling) {
-      retellWebClient.stopCall();
-    } else {
-      const registerCallResponse = await registerCall(agentId);
-      if (registerCallResponse.access_token) {
-        retellWebClient
-          .startCall({
-            accessToken: registerCallResponse.access_token,
-          })
-          .catch(console.error);
-        setIsCalling(true);
-      }
-    }
-  };
-
-  const delay = (ms: number) => new Promise((resolve) => setTimeout(resolve, ms));
 
   async function registerCall(agentId: string): Promise<RegisterCallResponse> {
     try {
@@ -223,91 +191,7 @@ const Call = ({
   // }
 
   return (
-    // <div>
-    //   {showRizzScore ? (
-    //     <div className="text-center mt-8">
-    //       {isLoadingAnalysis ? (
-    //         // Show loading GIF while analysis is being fetched
-    //         <div className="spinner-container">
-    //           <img
-    //             src="/assets/queennevercry.gif"
-    //             alt="Queen never cry"
-    //             className="mx-auto w-auto h-60"
-    //           />
-    //           <p className="mt-4 text-lg italic">WARNING: Rizz results might make you cry<br />But queen never cry</p>
-    //         </div>
-    //       ) : (
-    //         // Show rizz results once it's ready
-    //         <div className="rizz-feedback-container">
-    //           <p className="text-5xl text-white font-bold mb-4">
-    //             Rizz Results
-    //           </p>
-    //           <div className="rizz-feedback-box bg-gray-800 px-6 py-4 rounded-2xl"
-    //             style={{
-    //               maxWidth: "40rem",
-    //               margin: "0 auto",
-    //               whiteSpace: "pre-wrap",
-    //               wordWrap: "break-word",
-    //             }}>
-    //             <p className="text-lg text-white">{rizzMessage}</p>
-    //           </div>
-    //         </div>
-    //       )}
-    //       <Link href="/" passHref>
-    //         <button className="bg-[#BE4DFD] hover:bg-[#CC72FF] text-white py-2 px-6 rounded-full mt-8">
-    //           End Gooning Session
-    //         </button>
-    //       </Link>
-    //     </div>
-    //   ) : (
-    //     <div className="text-center">
-    //       {loading && (
-    //         <>
-    //           <p className="text-lg mb-4 italic">Connecting to Giga Chad...</p>
-    //           <button
-    //             onClick={toggleConversation}
-    //             className="bg-gray-700 hover:bg-gray-600 text-white px-6 py-3 mt-3 rounded-full"
-    //           >
-    //             Calling
-    //           </button>
-    //         </>
-    //       )}
-  
-    //       {showListeningText && !loading && (
-    //         <>
-    //           <div
-    //             className="transcript-box text-center"
-    //             style={{
-    //               maxWidth: "40rem",
-    //               margin: "0 auto",
-    //               whiteSpace: "pre-wrap",
-    //               wordWrap: "break-word",
-    //             }}
-    //           >
-    //             <p className="text-lg mb-4 italic">{transcriptContent || "Giga Chad is about to grace you with his presence"}</p>
-    //           </div>
-  
-    //           {showHangUpButton && (
-    //             <button
-    //               onClick={toggleConversation}
-    //               className="bg-gray-700 hover:bg-gray-600 text-white px-6 py-3 mt-3 rounded-full"
-    //             >
-    //               Hang Up
-    //             </button>
-    //           )}
-    //         </>
-    //       )}
-  
-    //       {isLoadingAnalysis && (
-    //         <div className="spinner-container">
-    //           <div className="spinner"></div>
-    //           <p className="mt-4 text-lg">Fetching rizz results...</p>
-    //         </div>
-    //       )}
-    //     </div>
-    //   )}
-    // </div>
-    <h1>Hi</h1>
+    <h1>Rubber Ducky Debugging Assistant</h1>
   );
 };
 
